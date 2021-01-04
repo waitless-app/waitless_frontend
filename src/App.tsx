5520b20fc5398b3fa9d9a8d0743bc6e6fbb6ef6e
swrzalek <stian.uek@gmail.com>
2021-01-04 16:14:43 +0100

    fixed stash drop

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
-import { ReactQueryDevtools } from "react-query/devtools";
+import { ReactQueryDevtools } from 'react-query/devtools';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
-} from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
-import Login from "./components/Login";
+  Redirect,
+} from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import { getItem } from './utils/localstorage';
+
+interface PrivateRouteProps {
+  component: any,
+  authed: String | Boolean
+  [key: string]: any,
+}
+
+function PrivateRoute({ component: Component, authed, ...rest } : PrivateRouteProps) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => (authed === true
+        ? <Component {...props} />
+        : <Redirect to={{ pathname: '/login' }} />)}
+    />
+  );
+}
 
 const queryClient = new QueryClient();
+const isAuthenticated = !!getItem('access_token');
 
 function App() {
-
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
-      <Switch>
-        <Route exact path="/" component={Login} />
-      </Switch>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <PrivateRoute authed={isAuthenticated} path="/" component={Dashboard} />
+        </Switch>
       </Router>
       <ReactQueryDevtools initialIsOpen />
     </QueryClientProvider>
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,71 @@
+import React from 'react';
+import {
+  Row, Col, Form, Input, Button, Checkbox,
+} from 'antd';
+import { History } from 'history';
+import AuthService from '../services/jwt.service';
+import { setItem } from '../utils/localstorage';
+
 interface LoginProps {
-    token: string;
+  token?: string,
+  history: History,
 }
 
-const Login: React.FC<LoginProps> = (props) => <h1>Hello, {props.token}</h1>
+const Login: React.FC<LoginProps> = ({ history }) => {
+  const onLoginSuccess = (data: { access: string, refresh: string}) => {
+    setItem('access_token', data.access);
+    setItem('refresh_token', data.refresh);
+    history.push('/dashboard');
+  };
+
+  const onFinish = (values: {email: string, password: string, remember: boolean}) => {
+    console.log(values);
+    AuthService.login({ email: values.email, password: values.password })
+      .then(({ data }) => onLoginSuccess(data));
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <Row align="middle" justify="center" style={{ height: '100vh' }}>
+      <Col span={6}>
+        <Form
+          name="basic"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+        >
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: 'Please input your username!' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }} name="remember" valuePropName="checked">
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Col>
+    </Row>
+  );
+};
 
 export default Login;
