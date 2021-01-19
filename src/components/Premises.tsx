5db23b1db62b0deacf4868c3f72efcd91e02dba9
swrzalek <stian.uek@gmail.com>
2021-01-19 23:33:47 +0100

    Add table for premises view to display all premises

--- src/components/Premises.tsx
+++ src/components/Premises.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import { RouteComponentProps } from 'react-router-dom';
+import {
+  Table, Space, Button, Row, Col,
+} from 'antd';
 import { PremisesService } from '../services/api.service';
 
 const Premises: React.FunctionComponent<RouteComponentProps> = () => {
@@ -15,11 +18,47 @@ const Premises: React.FunctionComponent<RouteComponentProps> = () => {
   if (error) {
     return (<div>{`An error has occurred: ${error.message}`}</div>);
   }
+  const columns = [
+    {
+      title: 'id',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'owner',
+      dataIndex: 'owner',
+      key: 'owner',
+    },
+    {
+      title: 'city',
+      dataIndex: 'city',
+      key: 'city',
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: () => (
+        <Space size="middle">
+          <Button type="primary">Edit</Button>
+          <Button danger>Remove</Button>
+        </Space>
+      ),
+    }];
 
   return (
     <div>
       <>
-        {data?.data.map((x: any) => x.name)}
+        <Row justify="end">
+          <Col style={{ marginBottom: '1em' }}>
+            <Button type="primary">Create New Premises</Button>
+          </Col>
+        </Row>
+        <Table columns={columns} dataSource={data?.data} />
       </>
     </div>
   );
