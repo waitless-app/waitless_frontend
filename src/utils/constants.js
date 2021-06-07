2397f39f5bd08c6e7b237ef2c482cb494b06a48d
Sebastian Wrzalek <s.wrzalek@gmail.com>
2021-06-08 00:20:09 +0200

    Create QR code component

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,4 @@
-export const API_URL = process.env.REACT_APP_API_URL || 'http://vps-ddebb6d7.vps.ovh.net:8000/api/';
+// export const API_URL = 'http://192.168.2.11:8000/api/';
+// export const API_URL = 'http://localhost:8000/api/';
+export const API_URL = 'http://vps-ddebb6d7.vps.ovh.net:8000/api/';
 export default { API_URL };
