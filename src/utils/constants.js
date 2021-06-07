87cbde8de7c26d8ad68e5ec93af32ac1a153e3a6
Sebastian Wrzalek <s.wrzalek@gmail.com>
2021-06-07 17:10:43 +0200

    add .env to gitignore

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,2 @@
-// export const API_URL = 'http://192.168.2.11:8000/api/';
-export const API_URL = 'http://localhost:8000/api/';
-// export const API_URL = 'https://cors-anywhere.herokuapp.com/http://vps-ddebb6d7.vps.ovh.net:8000/api/';
+export const API_URL = process.env.REACT_APP_API_URL || 'http://vps-ddebb6d7.vps.ovh.net:8000/api/';
 export default { API_URL };
