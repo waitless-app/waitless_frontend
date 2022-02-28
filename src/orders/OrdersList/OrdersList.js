149294fabe8b23bda55cbbaef1ad142af3d101df
Sebastian Wrzalek <s.wrzalek@gmail.com>
2022-02-28 23:44:44 +0100

    feat(order) Make table sortable

--- src/orders/OrdersList/OrdersList.js
+++ src/orders/OrdersList/OrdersList.js
@@ -4,10 +4,38 @@ import {
   Table,
 } from 'antd';
 import PropTypes from 'prop-types';
+import Avatar from 'antd/es/avatar/avatar';
 import { parseDate } from '../../utils/utils';
 import { EditableCell, EditableRow } from './OrderListEditableRow';
 
 const { Panel } = Collapse;
+
+const orderProductsNestedTable = ({ order_products: orderProducts }) => {
+  const columns = [
+    {
+      title: 'Product Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: (text, record) => (<div>{record.product.name}</div>),
+    },
+    {
+      title: 'Image',
+      dataIndex: 'image',
+      key: 'image',
+      render: (text, record) => (
+        <Avatar src={record.product.image} />
+      ),
+    },
+    {
+      title: 'Quantity',
+      dataIndex: 'quantity',
+      key: 'quantity',
+      render: (text, record) => (<div>{record.quantity}</div>),
+    },
+  ];
+
+  return <Table columns={columns} dataSource={orderProducts} pagination={false} />;
+};
 
 const OrdersList = ({ orders, handleStatusChange }) => {
   const [completedOrders, setCompletedOrders] = useState([]);
@@ -42,6 +70,7 @@ const OrdersList = ({ orders, handleStatusChange }) => {
       render: (text, record) => (
         <div>{ parseDate(record.created)}</div>
       ),
+      sorter: (a, b) => new Date(a.created).getTime() - new Date(b.created).getTime(),
     },
     {
       title: 'Last Update',
@@ -50,6 +79,7 @@ const OrdersList = ({ orders, handleStatusChange }) => {
       render: (text, record) => (
         <div>{ parseDate(record.updated)}</div>
       ),
+      sorter: (a, b) => new Date(a.updated).getTime() - new Date(b.updated).getTime(),
     }];
 
   const components = {
@@ -62,7 +92,6 @@ const OrdersList = ({ orders, handleStatusChange }) => {
     if (!col.editable) {
       return col;
     }
-
     return {
       ...col,
       onCell: (record) => ({
@@ -79,7 +108,14 @@ const OrdersList = ({ orders, handleStatusChange }) => {
     <>
       <Collapse defaultActiveKey={['1']} ghost>
         <Panel header="Active Orders" key="1">
-          <Table columns={columns1} components={components} dataSource={activeOrders} />
+          <Table
+            columns={columns1}
+            components={components}
+            dataSource={activeOrders}
+            rowKey={(record) => record.id}
+            pagination={false}
+            expandable={{ expandedRowRender: (record) => orderProductsNestedTable(record) }}
+          />
         </Panel>
         <Panel header="Completed Orders" key="2">
           <Table columns={columns} dataSource={completedOrders} />
